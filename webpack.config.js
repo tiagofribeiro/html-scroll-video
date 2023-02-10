7637799c3e45e82423599e92efd6ae4cb37c5c7d
tiagofribeiro <tiagofr98@gmail.com>
2023-02-10 16:53:54 -0300

    webpack with react config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path, { join } from 'path';
 import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -13,24 +14,58 @@ export default {
         rules: [
             {
                 test: /\.(js)$/,
-                exclude: /node_modules/,
+                // exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 },
                 resolve: {
-                  fullySpecified: false,
+                    fullySpecified: false,
                 }
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|mp4)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: true
+                        }
+                    }
+                ]
             }
         ],
     },
     "output": {
-        "path": __dirname + '/dist',
+        "path": __dirname + '/public',
         "filename": "bundle.js",
+        "publicPath": '/',
     },
     devServer: {
-        contentBase: join(__dirname, 'dist')
+        port: 3030,
+        historyApiFallback: true,
+        open: true,
+    },
+    externals: {
+        'react': 'React'
     },
     resolve: {
-        extensions: [".ts", ".js"],
+        extensions: [".ts", ".js", ".jsx"],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ]
 }
